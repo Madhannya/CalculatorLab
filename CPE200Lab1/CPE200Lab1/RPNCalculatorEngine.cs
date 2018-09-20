5ec781d8cc33580dd8b46016def013c6c30ed75e
Madhannya <35358377+Madhannya@users.noreply.github.com>
2018-09-20 16:58:50 +0700

    - สามารถ ใช้ RPN และเเกบัคแล้ว

--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -6,11 +6,34 @@ using System.Threading.Tasks;
 
 namespace CPE200Lab1
 {
-    public class RPNCalculatorEngine :CalculatorEngine
+    public class RPNCalculatorEngine : CalculatorEngine
     {
         public string Process(string str)
         {
             // your code here
+            string first;
+            string second;
+            string op;
+            Stack<string> numberstore = new Stack<string>();
+            string [] NumberCo;
+            NumberCo = str.Split(' ');
+            if (NumberCo.Length < 3) return "E";
+            for(int i=0;i< NumberCo.Length; i++)
+            {
+                if (!isOperator(NumberCo[i]))
+                {
+                    numberstore.Push(NumberCo[i]);
+                }
+                else if (numberstore.Count >= 2)
+                {
+                    op = NumberCo[i];
+                    second = numberstore.Pop();
+                    first = numberstore.Pop();
+                    numberstore.Push(calculate(op, first, second, 8));
+                }
+                else return "E";
+            }
+             if(numberstore.Count == 1)return numberstore.Pop();
             return "E";
         }
     }
